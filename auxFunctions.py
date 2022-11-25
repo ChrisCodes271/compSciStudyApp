21d392c94c4b76cf673f1ec3e69e1d89e9f7f412
ChrisCodes271 <Chris.r.walker271@outlook.com>
2022-11-24 19:38:10 -0500

    Modified DB Functions to return 1 Question, and Unique answers; of which one is correct.
    Refactoring to follow. Happy Thanksgiving!

--- auxFunctions.py
+++ auxFunctions.py
@@ -20,7 +20,7 @@ def delete_test_bank():  # Function to delete existing test bank. Called in the 
 
 
 def new_flashcard():  # New flashcard function used in the add flash card button.
-                      # Then pass to database function.  Request user input for card front and card back.
+    # Then pass to database function.  Request user input for card front and card back.
     try:
         card_front = simpledialog.askstring(title='Card Front',
                                             prompt='Whats on the front of your flashcard?', )
@@ -44,8 +44,27 @@ def delete_flashcard():  # Request what card to erase. Currently, requires SQL E
 
 def new_question():
     try:
-        var = databaseFunctions.retrieve_random_cardfront()
-        messagebox.showinfo(title='Card',message=str(var))
+        correct_card = databaseFunctions.retrieve_random_card()
+        # question = correct_card[0]
+        correct_answer = correct_card[1]
+        other_answers = dict()
+        other_answers[0] = ''
+        other_answers[1] = ''
+        other_answers[2] = ''
+        i = 3
+        j = 0
+        while i > 0:
+            test_value = databaseFunctions.retrieve_random_answer()
+            if test_value != correct_answer:
+                if test_value != other_answers[0] and test_value != other_answers[1] and test_value != other_answers[2]:
+                    other_answers[j] = test_value
+                    i -= 1
+                    j += 1
+        print(other_answers)
+        print(correct_answer)
 
     except Exception as e:
         messagebox.showerror(title="An Error Occurred", message=str(e))
+
+
+new_question()
--- databaseFunctions.py
+++ databaseFunctions.py
@@ -1,6 +1,4 @@
-from tkinter import *
 import sqlite3
-from tkinter import messagebox
 
 
 def create_table():  # Generate FLASHCARDS table. Set CardID, and Card front and back parameters for entries
@@ -41,30 +39,28 @@ def delete_entry(card_id):  # Pass cards ID from buttons module delete_flashcard
     conn.close()
 
 
-def retrieve_random_cardfront():
+def retrieve_random_card():
     conn = sqlite3.connect('flashcards.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT CardFront FROM FLASHCARDS ORDER BY RANDOM() LIMIT 1;")
-    result = cursor.fetchall()
-    conn.close()
-    print(type(result))
-    messagebox.showinfo(title='Test', message=''.join(result[0]))
+    cursor.execute("""SELECT * FROM FLASHCARDS ORDER BY RANDOM() LIMIT 1;""")
+    records = cursor.fetchall()
+    correct_card = dict()
+    for row in records:
+        correct_card[0] = row[1]
+        correct_card[1] = row[2]
+    cursor.close()
+    return correct_card
 
 
-def retrieve_random_cardback():
+def retrieve_random_answer():
+    card_back = ''
     conn = sqlite3.connect('flashcards.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT CardBack FROM FLASHCARDS ORDER BY RANDOM() LIMIT 3;")
-    test_var = cursor.fetchall()
+    cursor.execute("""SELECT * FROM FLASHCARDS ORDER BY RANDOM() LIMIT 3;""")
+    records = cursor.fetchall()
+    for row in records:
+            card_back = row[2]
     conn.close()
-    return test_var
+    return card_back
 
 
-window = Tk()
-button = Button(window,
-                text='Test Button',
-                command=retrieve_random_cardfront,
-                )
-button.pack()
-
-window.mainloop()
