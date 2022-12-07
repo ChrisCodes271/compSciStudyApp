d0da6426a6d68667c93022dd621807f3c55bb510
Chris Walker <chris.r.walker271@outlook.com>
2022-12-06 20:00:46 -0500

    Refactored front end utilizing customtkinter. Menubar now created, and entire buttons module was rendered obsolete. Deleted buttons module. Next step will be integration of quiz function on root frame.

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from tkinter import *
-import buttons
+import auxFunctions
+import customtkinter
 
-window = Tk()
-window.geometry("1400x1000")
-window.title("Roadmap.sh FlashCards (CompSci)")
-icon = PhotoImage(file='logo.png')
-window.iconphoto(TRUE, icon)
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-# Section of buttons. Referenced functions can be found in buttons module.
-buttons.add_test_bank_button(window)
-buttons.delete_test_bank_button(window)
-buttons.add_flashcard_button(window)
-buttons.delete_flashcard_button(window)
-buttons.new_question_button(window)
+root = customtkinter.CTk() # Initialize main window
+root.geometry(f"{800}x{800}") # Set size to 800 x 800
+root.title("Roadmap.sh Study Utility") # App title
+icon = PhotoImage(file='logo.png') # Set icon variable
+root.iconphoto(TRUE, icon) # Icon support on Windows (Currently does nothing on Mac (Unix based OS's))
 
-window.mainloop()
+frame = customtkinter.CTkFrame(master=root) # Generate frame for future use. 
+frame.pack(pady=20, padx=40, fill="both", expand=True) # Set padding from main window, and pack
+
+# Menu bar settings. 
+menu_bar = Menu(frame, background='purple', foreground='black', activebackground='black', activeforeground='purple')
+
+# Settings for "File" tab of the menu bar. 
+file = Menu(menu_bar,tearoff=0)
+file.add_command(label="New Test Bank", command=auxFunctions.create_test_bank)
+file.add_command(label="Delete Test Bank", command=auxFunctions.delete_test_bank)
+menu_bar.add_cascade(label="Database",menu=file)
+
+# Settings for "Edit" tab of the menu bar
+edit = Menu(menu_bar, tearoff=0)
+edit.add_command(label="New Flash Card",command=auxFunctions.new_flashcard)
+edit.add_command(label="Delete Flash Card",command=auxFunctions.delete_flashcard)
+menu_bar.add_cascade(label="Edit",menu=edit)
+
+root.config(menu=menu_bar)
+root.mainloop()
